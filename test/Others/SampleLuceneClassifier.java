d0105d9c9e253bd683592831204064d07628ea5a
sounakban <sounakban.science@gmail.com>
2017-02-15 18:41:06 +0530

    Transfer to new project

--- test/Others/SampleLuceneClassifier.java
+++ test/Others/SampleLuceneClassifier.java
@@ -3,7 +3,7 @@
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
 */
-package CopulaResources;
+package Others;
 import org.apache.lucene.classification.Classifier;
 import org.apache.lucene.classification.ClassificationResult;
 
